42c7079fa04f1e1ef3ec504226709902e487a0b5
Runidan <rusin.dn+github@gmail.com>
2023-02-13 13:51:48 +0500

    feat: time_format

--- rack/time_format.rb
+++ rack/time_format.rb
@@ -1,17 +1,29 @@
 class TimeFormat
-  FORMATES = %w[year month day hour minute second].freeze
+  RIGHT_FORMATES = %w[year month day hour minute second].freeze
 
   attr_reader :status, :body
 
-  def initialize(format)
-
+  def initialize(formats)
+    @formats = formats.split(",")
+    return error_formats if wrong_formats.any?
+    answer
   end
 
-  def status 
-    200
+  private
+
+  def wrong_formats
+    @wrong_formats = @formats - RIGHT_FORMATES
   end
 
-  def body
-    ['body-timeFormat']
+  def error_formats
+    @status = 400
+    @body = ["Unknown time format #{@wrong_formats}\n"] 
+  end
+
+  def answer
+    @status = 200
+    time = Time.now
+    time_params = @formats.map { |format| time_now.send(format.to_sym) }.join('-')
+    @body = ["#{time_params}\n"]
   end
 end
